c7ab20e5e76d580c1dfe43ae07ba21e1d7550b8e
lyrahgames <lyrahgames@mailbox.org>
2020-11-10 21:10:25 +0100

    Add Monte-Carlo pi computation

--- main.cpp
+++ main.cpp
@@ -11,7 +11,14 @@ int main() {  //
     return dist(rng);
   };
 
-  for (int i = 0; i < 10; ++i)  //
-    // cout << dist(rng) << '\n';
-    cout << random() << '\n';
+  int n = 10000000;
+  int c = 0;
+  for (int i = 0; i < n; ++i) {
+    const auto x = random();
+    const auto y = random();
+    const auto r2 = x * x + y * y;
+    if (r2 < 1) ++c;
+  }
+  auto monte_carlo_pi = 4 * float(c) / float(n);
+  cout << "pi = " << monte_carlo_pi << '\n';
 }
