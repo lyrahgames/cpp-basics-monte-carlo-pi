f545d9644a9d473330e6a84558a74d747086e9cb
lyrahgames <lyrahgames@mailbox.org>
2020-11-17 20:14:38 +0100

    Tweak the code

--- main.cpp
+++ main.cpp
@@ -4,21 +4,17 @@
 using namespace std;
 
 int main() {  //
-  random_device rd{};
-  mt19937 rng{rd()};
+  mt19937 rng{random_device{}()};
   uniform_real_distribution<float> dist{0, 1};
-  auto random = [&]() {  //
-    return dist(rng);
-  };
+  auto random = [&]() { return dist(rng); };
 
-  int n = 10000000;
+  const int n = 10000000;
   int c = 0;
   for (int i = 0; i < n; ++i) {
     const auto x = random();
     const auto y = random();
-    const auto r2 = x * x + y * y;
-    if (r2 < 1) ++c;
+    if ((x * x + y * y) < 1) ++c;
   }
-  auto monte_carlo_pi = 4 * float(c) / float(n);
+  const auto monte_carlo_pi = 4.0f * c / n;
   cout << "pi = " << monte_carlo_pi << '\n';
 }
